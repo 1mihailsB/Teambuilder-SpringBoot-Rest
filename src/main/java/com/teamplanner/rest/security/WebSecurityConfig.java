1f063067e5dc13be2dd223097bf9d7d9138f39fd
Michael <1mihails.b@gmail.com>
2020-03-09 16:11:39 +0100

    First commit
    Added to starter Spring-Boot project: basic CORS and Security settings,
    LoginController - receives Google Authorization code from Front End app,
    verifies this token with Google's endpoint, returns user's name
    to Front end app if successful, error if failed to verify the code.

---
+++ src/main/java/com/teamplanner/rest/security/WebSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.teamplanner.rest.security;
+
+
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthorizationCodeAuthenticationProvider;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+ 	protected void configure(HttpSecurity http) throws Exception {
+ 		http.cors().and().csrf().disable()
+ 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+ 		.and()
+ 		.authorizeRequests().antMatchers("/**").permitAll()
+ 		.antMatchers("/sec").permitAll()
+ 		.and().httpBasic();
+ 	}
+	
+	 @Bean
+	    public CorsConfigurationSource corsConfigurationSource() {
+	        CorsConfiguration configuration = new CorsConfiguration();
+	        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+	        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+	        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+	        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	        source.registerCorsConfiguration("/**", configuration);
+	        return source;
+	    }
+	
+	OAuth2AuthorizationCodeAuthenticationProvider c;
+
+ 	@Override
+ 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+ 		auth.inMemoryAuthentication().withUser("user").password("{noop}password").roles("USER");
+ 	}
+ 	
+	
+}
