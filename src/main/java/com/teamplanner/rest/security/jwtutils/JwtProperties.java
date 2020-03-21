92ec3dbd8848185e2a1a214ac40b0194a77946f6
Mihails B <1mihails.b@gmail.com>
2020-03-21 19:48:46 +0100

    Renamed a constant

--- src/main/java/com/teamplanner/rest/security/jwtutils/JwtProperties.java
+++ src/main/java/com/teamplanner/rest/security/jwtutils/JwtProperties.java
@@ -2,7 +2,7 @@ package com.teamplanner.rest.security.jwtutils;
 
 public class JwtProperties {
 	public static final String SECRET ="development";
-	public static final int EXPIRATION_TIME = 180_000; //3 minutes in milliseconds
+	public static final int EXPIRATION_TIME_MILLISECONDS = 60*10*1000; //10 minutes in milliseconds
 	public static final String TOKEN_PREFIX = "Bearer:";
 	public static final String COOKIE_NAME = "Authorization";
 	
