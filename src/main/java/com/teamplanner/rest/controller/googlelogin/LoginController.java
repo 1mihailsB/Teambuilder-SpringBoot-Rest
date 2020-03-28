a62537cd62a3f72f2c2d4431868f7ec51a46a3bc
Mihails B <1mihails.b@gmail.com>
2020-03-28 23:49:23 +0100

    Added nickname choice on first log-in

--- src/main/java/com/teamplanner/rest/controller/googlelogin/LoginController.java
+++ src/main/java/com/teamplanner/rest/controller/googlelogin/LoginController.java
@@ -1,15 +1,21 @@
 package com.teamplanner.rest.controller.googlelogin;
 
+import com.teamplanner.rest.model.User;
 import com.teamplanner.rest.security.jwtutils.JwtProperties;
+import com.teamplanner.rest.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.util.WebUtils;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.lang.invoke.MethodHandles;
 import java.util.Map;
@@ -20,12 +26,14 @@ import java.util.Map;
 public class LoginController {
 
 	private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-	
+
+	UserService userService;
     GoogleAuthResponseParser garp;
 
     @Autowired
-    public LoginController(GoogleAuthResponseParser garp) {
+    public LoginController(GoogleAuthResponseParser garp, UserService userService) {
         this.garp = garp;
+        this.userService = userService;
     }
 
     @PostMapping("/authCode")
@@ -57,9 +65,39 @@ public class LoginController {
         return "LoggedOut";
     }
 
-    @PostMapping("/chooseNickname")
-    public ResponseEntity<Map> chooseNickname(HttpServletResponse response){
+    @PutMapping("/chooseNickname")
+    public String chooseNickname(@RequestBody String nickname, Authentication authentication,
+                                              HttpServletRequest request, HttpServletResponse response){
 
-        return null;
+        User user = userService.findById((String) authentication.getPrincipal());
+
+        if(user != null && user.getNickname() != nickname){
+            user.setNickname(nickname);
+
+            //also refresh authorization and nickname cookie after updating user nickname
+            try{
+                user = userService.save(user);
+
+                Cookie userNickNameCookie = WebUtils.getCookie(request, "nickname");
+                userNickNameCookie.setValue(nickname);
+                userNickNameCookie.setMaxAge(JwtProperties.EXPIRATION_TIME_MILLISECONDS/1000);
+                userNickNameCookie.setPath("/");
+                response.addCookie(userNickNameCookie);
+
+                Cookie userJwtCookie = WebUtils.getCookie(request, JwtProperties.COOKIE_NAME);
+                userJwtCookie.setSecure(true);
+                userJwtCookie.setPath("/");
+                userJwtCookie.setMaxAge(JwtProperties.EXPIRATION_TIME_MILLISECONDS/1000);
+
+                response.addCookie(userJwtCookie);
+            }catch(DataIntegrityViolationException e){
+                if (LOG.isDebugEnabled()) LOG.debug(e.getMessage());
+                System.out.println("Nickname update exception");
+                return "Username taken";
+            }
+
+        }
+
+        return "Username changed";
     }
 }
