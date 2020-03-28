891bef75c49afc17d871e7f3dec0627f60829cb7
Mihails B <1mihails.b@gmail.com>
2020-03-29 00:54:47 +0100

    Update to sql script

--- create-user-table.sql
+++ create-user-table.sql
@@ -1,10 +1,12 @@
 use website_rest;
 
+drop table if exists users;
+
 CREATE TABLE users (
   googlesub varchar(255) NOT NULL,
   name varchar (50) NOT NULL,
   email varchar (50) NOT NULL,
-  nickname varchar (16) NOT NULL,
+  nickname varchar (16) NOT NULL UNIQUE,
   enabled tinyint(1) NOT NULL,
   roles VARCHAR (100) NOT NULL,
   permissions VARCHAR (200) NOT NULL,
