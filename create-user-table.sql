2af496a97bedbb7dc65c579d353e452b57cc1c78
Mihails B <1mihails.b@gmail.com>
2020-03-30 20:17:30 +0200

    Changed nickname constraints to allow null for user who haven't yet chosen a nickname

--- create-user-table.sql
+++ create-user-table.sql
@@ -3,12 +3,13 @@ use website_rest;
 drop table if exists users;
 
 CREATE TABLE users (
-  googlesub varchar(255) NOT NULL,
+  googlesub varchar(255) UNIQUE NOT NULL,
   name varchar (50) NOT NULL,
   email varchar (50) NOT NULL,
-  nickname varchar (16) NOT NULL UNIQUE,
+  nickname varchar (16) UNIQUE,
   enabled tinyint(1) NOT NULL,
   roles VARCHAR (100) NOT NULL,
   permissions VARCHAR (200) NOT NULL,
   PRIMARY KEY (googlesub)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
