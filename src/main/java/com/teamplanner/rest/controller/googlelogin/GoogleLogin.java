46c700adaf0dbe6df37df623391b6c43d96c23d3
Mihails B <1mihails.b@gmail.com>
2020-03-31 21:36:59 +0200

    Refactor

--- src/main/java/com/teamplanner/rest/controller/googlelogin/GoogleLogin.java
+++ src/main/java/com/teamplanner/rest/controller/googlelogin/GoogleLogin.java
@@ -24,16 +24,17 @@ import java.util.stream.Collectors;
 @Component
 public class GoogleLogin {
     private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-  
-    GoogleAuthorizationCodeExchange exchange;
-    UserService userService;
-    JwtGeneratorVerifier jwtgv;
+
+    private final GoogleAuthorizationCodeExchange exchange;
+    private final UserService userService;
+    private final JwtGeneratorVerifier jwtGeneratorVerifier;
 
     @Autowired
-    public GoogleLogin(GoogleAuthorizationCodeExchange gace, UserService userService, JwtGeneratorVerifier jwtgv) {
-    	this.exchange = gace;
+    public GoogleLogin(GoogleAuthorizationCodeExchange exchange, UserService userService,
+                       JwtGeneratorVerifier jwtGeneratorVerifier) {
+    	this.exchange = exchange;
     	this.userService = userService;
-    	this.jwtgv = jwtgv;
+    	this.jwtGeneratorVerifier = jwtGeneratorVerifier;
 	}
     @SuppressWarnings("rawtypes")
     protected ResponseEntity<Map> login(Map<String, Object> authorizationCode, HttpServletResponse httpResponse) {
@@ -43,46 +44,25 @@ public class GoogleLogin {
         googleResponse = exchange.exchangeAuthCode(authorizationCode);
 
         if (googleResponse.getStatusCode() == HttpStatus.OK) {
-            return authenticationAndResponse(googleResponse, httpResponse);
+            Map<String,String> googleUserInfo = extractGoogleUserInfo(googleResponse);
+            return authenticateAndPrepeareResponse(googleUserInfo, httpResponse);
         }
         throw new RuntimeException("an error occured while exchanging authorization code");
     }
 
     
     @SuppressWarnings("rawtypes")
-	private ResponseEntity<Map> authenticationAndResponse(ResponseEntity<Map> googleResponse, HttpServletResponse httpResponse) {
-    	
-    	JSONObject json = new JSONObject(googleResponse);
+	private ResponseEntity<Map> authenticateAndPrepeareResponse(Map<String,String> googleUserInfo, HttpServletResponse httpResponse) {
 
-        if (LOG.isDebugEnabled()) LOG.debug("googleresponse:  {}", json.toString(4));
-
-        String jwtIdToken = (String) googleResponse.getBody().get("id_token");
-        String[] splitToken = jwtIdToken.split("\\.");
-        String base64EncodedBody = splitToken[1];
-
-        Base64 base64UrlSafe = new Base64(true);
-        String idTokenDecoded = new String(base64UrlSafe.decode(base64EncodedBody));
-        if (LOG.isDebugEnabled()) LOG.debug("JWT Body : {}", new JSONObject(idTokenDecoded).toString(4));
-
-        String bodyQuotesRemoved = idTokenDecoded.replaceAll("\"", "");
-        String[] bodyEntries = bodyQuotesRemoved.split(",");
-
-        List<String> selectedUserProperties = Arrays.asList("sub","given_name", "email");
-        Map<String, String> userprops =
-                Arrays.stream(bodyEntries)
-                        .map(elem -> elem.split(":"))
-                        .filter(elem -> selectedUserProperties.contains(elem[0]))
-                        .collect(Collectors.toMap(e -> e[0], e -> e[1]));
-
-        User user = userService.findById(userprops.get("sub"));
+        User user = userService.findById(googleUserInfo.get("sub"));
         if(user == null) {
-        	user = new User(userprops.get("sub"), userprops.get("given_name"), userprops.get("email"));
+        	user = new User(googleUserInfo.get("sub"), googleUserInfo.get("given_name"), googleUserInfo.get("email"));
         	userService.save(user);
         }else {
         	if (LOG.isDebugEnabled()) LOG.debug("----- User already exists in our database: {}", user);
         }
 
-        String jwt = jwtgv.createSignedJwt(userprops.get("sub"));
+        String jwt = jwtGeneratorVerifier.createSignedJwt(googleUserInfo.get("sub"));
 
         final Cookie jwtCookie = new Cookie(JwtProperties.COOKIE_NAME, JwtProperties.TOKEN_PREFIX + jwt);
         jwtCookie.setHttpOnly(true);
@@ -106,6 +86,31 @@ public class GoogleLogin {
         if (LOG.isDebugEnabled()) LOG.debug("response to frontend: {}", new JSONObject(response).toString(4));
 
         return response;
+    }
+
+    private Map<String, String> extractGoogleUserInfo(ResponseEntity<Map> googleResponse){
+
+        if (LOG.isDebugEnabled()) LOG.debug("googleresponse:  {}",  new JSONObject(googleResponse).toString(4));
+
+        String jwtIdToken = (String) googleResponse.getBody().get("id_token");
+        String[] splitToken = jwtIdToken.split("\\.");
+        String base64EncodedBody = splitToken[1];
+
+        Base64 base64UrlSafe = new Base64(true);
+        String idTokenDecoded = new String(base64UrlSafe.decode(base64EncodedBody));
+        if (LOG.isDebugEnabled()) LOG.debug("JWT Body : {}", new JSONObject(idTokenDecoded).toString(4));
+
+        String bodyQuotesRemoved = idTokenDecoded.replaceAll("\"", "");
+        String[] bodyEntries = bodyQuotesRemoved.split(",");
+
+        List<String> wantedUserInfo = Arrays.asList("sub","given_name", "email");
+        Map<String, String> googleUserInfo =
+                Arrays.stream(bodyEntries)
+                        .map(elem -> elem.split(":"))
+                        .filter(elem -> wantedUserInfo.contains(elem[0]))
+                        .collect(Collectors.toMap(e -> e[0], e -> e[1]));
+
+        return googleUserInfo;
     }
     
 }
