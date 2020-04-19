fbf0e382914f262a7f64730bd734091e4a8f0a8d
Mihails B <1mihails.b@gmail.com>
2020-04-20 01:01:42 +0200

    Added entity to dto conversions.
    gamePlanDto for single gameplan, userFriendNicknames for nicknames of all friends of given user,
    nicknamesOfInvitingUsers for names of users who sent friend request to given user

--- src/main/java/com/teamplanner/rest/model/EntityDtoConverter.java
+++ src/main/java/com/teamplanner/rest/model/EntityDtoConverter.java
@@ -1,6 +1,7 @@
 package com.teamplanner.rest.model;
 
 import com.teamplanner.rest.model.dto.GamePlanDto;
+import com.teamplanner.rest.model.entity.Friendship;
 import com.teamplanner.rest.model.entity.GamePlan;
 import com.teamplanner.rest.model.entity.User;
 import org.springframework.beans.BeanUtils;
@@ -24,5 +25,43 @@ public class EntityDtoConverter {
             }
         }
         return gamePlansDto;
+    }
+
+    public GamePlanDto gameplanToDto (GamePlan gamePlan, User user){
+        GamePlanDto gamePlanDto = new GamePlanDto();
+        BeanUtils.copyProperties(gamePlan, gamePlanDto, "author");
+        gamePlanDto.setAuthorNickname(user.getNickname());
+
+        return gamePlanDto;
+    }
+
+    public List<String> userFriendNicknames (List<Friendship> initiatedFriendships, List<Friendship> invitedToFriendships){
+
+        List<String> friendNicknames = new ArrayList<>();
+
+        if(!initiatedFriendships.isEmpty()) {
+            for(Friendship friendship : initiatedFriendships){
+                friendNicknames.add(friendship.getInvitedUser().getNickname());
+            }
+        }
+        if(!invitedToFriendships.isEmpty()){
+            for(Friendship friendship : invitedToFriendships){
+                friendNicknames.add(friendship.getInvitingUser().getNickname());
+            }
+        }
+
+        return friendNicknames;
+    }
+
+    public List<String> nicknamesOfInvitingUsers(List<Friendship> incomingFriendRequests) {
+        List<String> invitingUsersNicknames = new ArrayList<>();
+
+        if(!incomingFriendRequests.isEmpty()){
+            for(Friendship friendship : incomingFriendRequests){
+                invitingUsersNicknames.add(friendship.getInvitingUser().getNickname());
+            }
+        }
+
+        return invitingUsersNicknames;
     }
 }
