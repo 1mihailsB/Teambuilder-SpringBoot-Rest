426cf27fc04349cd331550582a526054511674db
Mihails B <1mihails.b@gmail.com>
2020-04-20 00:52:39 +0200

    Added delete/get by id game endpoints. Added validation for gameplan fields.

--- src/main/java/com/teamplanner/rest/controller/GamePlanController.java
+++ src/main/java/com/teamplanner/rest/controller/GamePlanController.java
@@ -13,10 +13,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import javax.transaction.Transactional;
 import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RestController
 @CrossOrigin
@@ -34,9 +35,8 @@ public class GamePlanController {
         this.gamePlanService = gamePlanService;
     }
 
-    @GetMapping("/my")
-    @Transactional
-    public List<GamePlanDto> about (Authentication authentication) {
+    @GetMapping("/all")
+    public List<GamePlanDto> myGames (Authentication authentication) {
         String googlesub = (String) authentication.getPrincipal();
         User user = userService.findById(googlesub);
         
@@ -45,12 +45,48 @@ public class GamePlanController {
         return gamePlanDtos;
     }
 
+    @PatchMapping("/editById/{id}")
+    public String editGamePlan(@PathVariable int id, @RequestBody Map<String, String> mainText, Authentication authentication){
+
+        Pattern mainTextPattern = Pattern.compile("^[a-zA-Z0-9 ,:;'/!.\n\\[\\]!@_-]{1,3000}$");
+        Matcher mainTextMatcher = mainTextPattern.matcher(mainText.get("mainText"));
+
+        if(!mainTextMatcher.matches()){
+            return "Incorrect text";
+        }
+
+        String userGooglesub = (String) authentication.getPrincipal();
+        GamePlan gamePlan = gamePlanService.findById(id);
+
+        if(userGooglesub.equals(gamePlan.getAuthor().getGooglesub())){
+            gamePlan.setMainText(mainText.get("mainText"));
+            gamePlanService.save(gamePlan);
+            return "Game plan edited";
+        }
+
+        return "You are not the author of this plan";
+    }
+
     @PutMapping("/create")
-    public String createGamePlan(@RequestBody Map<String, Object> gamePlanForm, Authentication authentication){
+    public String createGamePlan(@RequestBody Map<String, String> gamePlanForm, Authentication authentication){
+
+        Pattern titlePattern = Pattern.compile("^[a-zA-Z0-9 ,:\\[\\]!@_-]{1,50}$");
+        Matcher titleMatcher = titlePattern.matcher(gamePlanForm.get("title"));
+
+        if(!titleMatcher.matches()){
+            return "Incorrect title";
+        }
+
+        Pattern mainTextPattern = Pattern.compile("^[a-zA-Z0-9 ,:;'/!.\n\\[\\]!@_-]{1,3000}$");
+        Matcher mainTextMatcher = mainTextPattern.matcher(gamePlanForm.get("mainText"));
+
+        if(!mainTextMatcher.matches()){
+            return "Incorrect text";
+        }
 
         User user = userService.findById((String) authentication.getPrincipal());
 
-        GamePlan gameplan = new GamePlan((String) gamePlanForm.get("title"), (String) gamePlanForm.get("mainText"),
+        GamePlan gameplan = new GamePlan(gamePlanForm.get("title"), (String) gamePlanForm.get("mainText"),
                 ZonedDateTime.now());
         gameplan.setAuthor(user);
 
@@ -63,14 +99,31 @@ public class GamePlanController {
         return "Game plan created";
     }
 
+    @GetMapping("/getById/{id}")
+    public GamePlanDto getGamePlan(@PathVariable int id, Authentication authentication){
+        String googlesub = (String) authentication.getPrincipal();
+        User user = userService.findById(googlesub);
+
+        GamePlan gamePlan = gamePlanService.findById(id);
+
+        String authorGooglesub = gamePlan.getAuthor().getGooglesub();
+
+        if(user.getGooglesub().equals(authorGooglesub)){
+            return entityDtoConverter.gameplanToDto(gamePlan, user);
+        }
+        throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+    }
+
     @DeleteMapping("/delete/{id}")
     public void deleteGamePlan(@PathVariable int id, Authentication authentication){
         String googlesub = (String) authentication.getPrincipal();
 
         GamePlan gamePlan = gamePlanService.findById(id);
+
         String authorGooglesub = gamePlan.getAuthor().getGooglesub();
 
         if(googlesub.equals(authorGooglesub)){
+
             gamePlanService.deleteById(id);
         }else {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN);
