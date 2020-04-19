f16e6ce298d20ba6f53cbfc7daf5f8ffe5a5f302
Mihails B <1mihails.b@gmail.com>
2020-04-20 00:55:11 +0200

    added find by nickname method to JpaRepository

--- src/main/java/com/teamplanner/rest/dao/UserRepository.java
+++ src/main/java/com/teamplanner/rest/dao/UserRepository.java
@@ -3,7 +3,11 @@ package com.teamplanner.rest.dao;
 import com.teamplanner.rest.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String>{
 
     User findByGooglesub(String username);
+
+    Optional<User> findByNickname(String nickname);
 }
