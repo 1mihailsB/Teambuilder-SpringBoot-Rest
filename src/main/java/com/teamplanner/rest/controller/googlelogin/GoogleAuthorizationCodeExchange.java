6c8a9d48f48b74b4db5e1be09158ba305b65d7d3
Mihails B <1mihails.b@gmail.com>
2020-04-20 00:58:53 +0200

    Change map type reference

--- src/main/java/com/teamplanner/rest/controller/googlelogin/GoogleAuthorizationCodeExchange.java
+++ src/main/java/com/teamplanner/rest/controller/googlelogin/GoogleAuthorizationCodeExchange.java
@@ -32,7 +32,7 @@ public class GoogleAuthorizationCodeExchange {
     }
 
     @SuppressWarnings("rawtypes")
-	public ResponseEntity<Map> exchangeAuthCode(Map<String, Object> authorizationCode) {
+	public ResponseEntity<Map> exchangeAuthCode(Map<String, String> authorizationCode) {
 
         ResponseEntity<Map> googleResponse = null;
 
@@ -49,7 +49,7 @@ public class GoogleAuthorizationCodeExchange {
                 + "&redirect_uri=" + redirectUri
                 + "&grant_type=authorization_code", headers);
 
-            googleResponse = restTemplate.exchange(tokenEndpoint, HttpMethod.POST, entity, Map.class);
+        googleResponse = restTemplate.exchange(tokenEndpoint, HttpMethod.POST, entity, Map.class);
 
         return googleResponse;
 
