47f7eb51d202d8d7eb5ad97aa1ef770a1cdf5d08
Mihails B <1mihails.b@gmail.com>
2020-05-14 02:21:50 +0200

    Add gameplan members table to sql script

--- mysql-schema.sql
+++ mysql-schema.sql
@@ -5,6 +5,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 drop table if exists users;
 drop table if exists gameplans;
 drop table if exists friendships;
+drop table if exists gameplan_members;
 
 CREATE TABLE users (
   `googlesub` varchar(255) UNIQUE NOT NULL,
@@ -26,7 +27,7 @@ CREATE TABLE gameplans (
     `creation_datetime` DATETIME not null,
     PRIMARY KEY (`id`),
     CONSTRAINT `FK_gameplans` FOREIGN KEY (`author_googlesub`)
-    REFERENCES `users` (`googlesub`) ON DELETE NO ACTION,
+    REFERENCES `users` (`googlesub`),
     CONSTRAINT `UQ_author_title_` UNIQUE(`author_googlesub`, `title`)
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -44,12 +45,10 @@ CHECK(`status` between 0 and 1),
 CHECK(`friend_1_googlesub`<>`friend_2_googlesub`),
 
 CONSTRAINT `FK_friend_1` FOREIGN KEY (`friend_1_googlesub`)
-REFERENCES `users` (`googlesub`)
-ON DELETE NO ACTION ON UPDATE NO ACTION,
+REFERENCES `users` (`googlesub`),
 
 CONSTRAINT `FK_friend_2` FOREIGN KEY (`friend_2_googlesub`)
 REFERENCES `users` (`googlesub`)
-ON DELETE NO ACTION ON UPDATE NO ACTION
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 delimiter $
@@ -66,5 +65,20 @@ begin
   end if;
 end$
 delimiter ;
+
+CREATE TABLE gameplan_members (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`gameplan_id` int(11) NOT NULL,
+`member_user_googlesub` varchar(255),
+`status` tinyint(1) DEFAULT 0,
+PRIMARY KEY (`id`),
+
+CONSTRAINT `FK1_game_members` foreign key (`gameplan_id`) references `gameplans` (`id`),
+CONSTRAINT `FK2_game_members` foreign key (`member_user_googlesub`) references `users` (`googlesub`),
+
+UNIQUE KEY `gameplan_member_combination` (`gameplan_id`, `member_user_googlesub`),
+
+CHECK(`status` between 0 and 1)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 SET FOREIGN_KEY_CHECKS = 1;
